5ec3158e437bff4802a6bf01715bc8b277aa6eb9
Sonali-S25 <69160246+Sonali-S25@users.noreply.github.com>
2020-08-04 07:23:54 -0700

    Update project_1.py

--- project_1.py
+++ project_1.py
@@ -2,40 +2,29 @@ import tkinter as tk
 import random
 from tkinter import *
 from PIL import Image,ImageTk
-
 def roll():
     img = PhotoImage(file = "dice1.png")
     img1 = PhotoImage(file = "dice2.png")
     img2 = PhotoImage(file = "dice3.png")
     img3 = PhotoImage(file = "dice4.png")
     img4 = PhotoImage(file = "dice5.png")
-    img5 = PhotoImage(file = "dice6.png")    
+    img5 = PhotoImage(file = "dice6.png")
     no = random.randint(1,6)
     if no == 1:
-        imglabel = Label(root, image=img)
-        imglabel.image=img
-        imglabel.pack() 
+        n=img1
     if no == 2:
-        imglabel1 = Label(root, image=img1)
-        imglabel1.image=img1
-        imglabel1.pack()
+        n=img1
     if no == 3:
-        imglabel2 = Label(root, image=img2)
-        imglabel2.image=img2
-        imglabel2.pack()
+        n=img2
     if no == 4:
-        imglabel3 = Label(root, image=img3)
-        imglabel3.image=img3
-        imglabel3.pack()
+        n=img3
     if no == 5:
-        imglabel4 = Label(root, image=img4)
-        imglabel4.image=img4
-        imglabel4.pack()
+        n=img4
     if no == 6:
-        imglabel5 = Label(root, image=img5)
-        imglabel5.image=img5
-        imglabel5.pack() 
-        
+        n=img5
+    imglabel = Label(root, image=n)
+    imglabel.image=n
+    imglabel.place(x=20, y=20)        
 root=tk.Tk()
 root.title("Dice Roll")
 button1 = tk.Button(root,text="Roll", command=roll)
@@ -43,8 +32,3 @@ button1.pack()
 button2 = tk.Button(root,text="Quit", command=root.destroy)
 button2.pack()
 root.mainloop()
-  
-
-
-        
-          
